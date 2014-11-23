a785adcd4eea24ef7cad9a9d1dbddb47f8fa55b2
Nick <nicholascgilpin@yahoo.com>
2014-11-22 19:43:44 -0600

    Added to next button and universal
    Fixed what caused the library files not to work

--- display.cpp
+++ display.cpp
@@ -1,6 +1,6 @@
 
 #include "universal.h"
-
+// You should clarify these magic numbers. See page 530 of textbook for demo.
 Display_Window::Display_Window(Point xy, int w, int h, const string& title):Window(xy,w,h,title),
     quit_button(Point(x_max()-70,0),70,20,"Quit",cb_quit),
 	next_button(Point(x_max()-70,30),70,20,"Next",cb_quit),
@@ -31,9 +31,14 @@ void Display_Window:: cb_previous(Address,Address pw)
 	{
 		reference_to<Display_Window>(pw).previous();
 	}
-// Actual Functions -------------------------------
-// Corrects index range errors
-// I might need to use qualified names for the vars...
+// Actual Functions ------------------------------------//
+set_search_mode(bool b)
+{
+	Display_Window::search_mode = b;
+	Display_Window::index = 0; // Always reset index when switching modes
+}
+/* check_index_range Corrects index range errors		//
+// I might need to use qualified names for the vars...  */
 bool check_index_range(int i)
 	{
 		if ((index < 0) || (index > int_limit))
@@ -47,27 +52,47 @@ bool check_index_range(int i)
 			return true; 
 		}
 	}
-/* // postponed until we get closer to this part----------------
 // Reads line of file, displays pic & tags, increments index
-
 void Display_Window::next()
 	{
 		check_index_range(index);
-		if (index = 0)
+		if (search_mode) // Searching
 		{
-			// get the picture from the first line of file
-			// attach picture 
-			// redraw
-			check_index_range(index); // prevent range errors
-			index += 1;
+			if (index == 0)
+			{
+				// if index = 0 then we're on the default picture
+				// get the picture from the first spot in vector
+				// attach picture 
+				// redraw
 			}
-		else if (index < 0)
-		{
-			// get picture from the next line of file
-			// attach picture 
-			// redraw
-			// increment browser index
+			if (index < 0)
+			{
+				// get picture from s_vector[i+1];
+				// attach picture 
+				// redraw
+				// increment browser index
+			}
 		}
+		if (!search_mode) // Browsing
+		{
+			if ()
+			{
+				// if index = 0 then we're on the default picture
+				// get the picture from the first line of file/
+				// attach picture 
+				// redraw
+			}
+			if (index < 0)
+			{
+				// get picture from the next line of file
+				// attach picture 
+				// redraw
+				// increment browser index
+			}
+		}
+
+		check_index_range(index); // prevent range errors
+		index += 1;			  	  // set to 0 when changing modes!
 		
 	}
 // Reads index-1 line of file, displays pic & tags, decrements index
@@ -76,6 +101,13 @@ void Display_Window::previous()
 		check_index_range(index);
 		// add code
 	}
+
+/* Nick's Todo list:
+Browse all pictures.
+When a picture is displayed, so are its tags.  
+Provide buttons to see the Next or Previous picture.
+Let buttons also work for search results
+Find pictures with any combination of tags. 
 */
 void Display_Window::quit()
 	{
--- input.cpp
+++ input.cpp
@@ -1,37 +1,37 @@
 #include "universal.h"
 
-istream& operator>>(istream& is, Data_obj& a) //when user uploads a new file
-    {
-        char ch1, ch2, ch3, ch4, ch5; //commas
-        is >> file_name >> ch1 >> tag[0] >> ch2 >> tag[1] >> ch3 >> tag[2] >> ch4 >> tag[3] >> ch5 >> tag[4]; //will work on tag vector later 
-        if(!is) return is;
-        if(ch1!=',' || ch2!=',' || ch3!=',' || ch4!=',' || ch5!=',') //format error
-        {
-            is.clear(ios_base::failbit);
-            return is;
-        }
-        
-        add = Data_obj(file_name, tag[0], tag[1], tag[2], tag[3], tag[4]); //update entry
-        
-        return is;
-    }
+istream& operator>>(istream& is, Data_obj& a) //when user adds a new file
+{
+	char ch1, ch2, ch3, ch4, ch5; //commas
+	is >> file_name >> ch1 >> tag[0] >> ch2 >> tag[1] >> ch3 >> tag[2] >> ch4 >> tag[3] >> ch5 >> tag[4]; //will work on tag vector later 
+	if (!is) return is;
+	if (ch1 != ',' || ch2 != ',' || ch3 != ',' || ch4 != ',' || ch5 != ',') //format error
+	{
+		is.clear(ios_base::failbit);
+		return is;
+	}
+
+	add = Data_obj(file_name, tag[0], tag[1], tag[2], tag[3], tag[4]); //update entry
+
+	return is;
+}
 //-----------------------------------------------------------------------------------------------
 //User will input file and tags
-if(URLstring)//if a URL exists
-    {
-        system((string("wget ") + URLstring + " –O " + file_name).c_str());
-        ofs.open("file_names.txt", ios_base::out);
-        ofs << file_name << ch1 << tag[0] << ch2 << tag[1] << ch3 << tag[2] << ch4 << tag[3] << ch5 << tag[4] << '\n';
-        ofs.close();
-    }
+if (URLstring)//if a URL exists
+{
+	system((string("wget ") + URLstring + " –O " + file_name).c_str());
+	ofs.open("file_names.txt", ios_base::out);
+	ofs << file_name << ch1 << tag[0] << ch2 << tag[1] << ch3 << tag[2] << ch4 << tag[3] << ch5 << tag[4] << '\n';
+	ofs.close();
+}
 // you can use to_lower so that you don't have to put different cases here
-else if(file_name.substr(file_name.find_last_of(".") + 1) == "jpg" || file_name.substr(file_name.find_last_of(".") + 1) == "jpeg" || file_name.substr(file_name.find_last_of(".") + 1) == "gif")
-    {
-        ofs.open("file_names.txt", ios_base::out);
-        ofs << file_name << ch1 << tag[0] << ch2 << tag[1] << ch3 << tag[2] << ch4 << tag[3] << ch5 << tag[4] << '\n';
-        ofs.close();
-    }
+else if (file_name.substr(file_name.find_last_of(".") + 1) == "jpg" || file_name.substr(file_name.find_last_of(".") + 1) == "jpeg" || file_name.substr(file_name.find_last_of(".") + 1) == "gif")
+{
+	ofs.open("file_names.txt", ios_base::out);
+	ofs << file_name << ch1 << tag[0] << ch2 << tag[1] << ch3 << tag[2] << ch4 << tag[3] << ch5 << tag[4] << '\n';
+	ofs.close();
+}
 else
-    {
-        //wrong file type
-    }
+{
+	//wrong file type
+}
