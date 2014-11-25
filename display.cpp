b37f12dea719535cd0847f84f4ae49196a3f8ecd
ntorres16 <ntorres@tamu.edu>
2014-11-25 15:55:43 -0600

    Buttons that don't do anything!

--- display.cpp
+++ display.cpp
@@ -2,19 +2,19 @@
 #include "universal.h"
 // You should clarify these magic numbers. See page 530 of textbook for demo.
 Display_Window::Display_Window(Point xy, int w, int h, const string& title):Window(xy,w,h,title),
-    quit_button(Point(x_max()-70,0),70,20,"Quit",cb_quit),
-	next_button(Point(x_max()-70,30),70,20,"Next",cb_quit),
-    previous_button(Point(x_max()-70,60),70,20,"Previous",cb_quit),
-    home_button(Point(x_max()-70,90),70,20,"Home",cb_quit),
-	family(Point(x_max()-600,480),70,20,"Family",cb_quit),
-	friends(Point(x_max()-520,480),70,20,"Friends",cb_quit),
-	aggieland(Point(x_max()-440,480),70,20,"Aggieland",cb_quit),
-	pets(Point(x_max()-360,480),70,20,"Pets",cb_quit),
-	vacation(Point(x_max()-280,480),70,20,"Vacation",cb_quit),
-	search_button(Point(x_max()-70,0),70,20,"Search",cb_quit),
-	add_tag_button(Point(x_max()-70,0),70,20,"Add",cb_quit),
-	add_url_button(Point(x_max()-70,0),70,20,"Add URL",cb_quit),
-	add_file_button(Point(x_max()-70,0),70,20,"Add File",cb_quit)
+ 	quit_button(Point(x_max()-70,0),70,20,"Quit",cb_quit),
+	next_button(Point(x_max()-70,30),70,20,"Next",cb_next),
+    previous_button(Point(x_max()-70,60),70,20,"Previous",cb_previous),
+    home_button(Point(x_max()-70,90),70,20,"Home",cb_home),
+	family(Point(x_max()-600,480),70,20,"Family",cb_family),
+	friends(Point(x_max()-520,480),70,20,"Friends",cb_friends),
+	aggieland(Point(x_max()-440,480),70,20,"Aggieland",cb_aggieland),
+	pets(Point(x_max()-360,480),70,20,"Pets",cb_pets),
+	vacation(Point(x_max()-280,480),70,20,"Vacation",cb_vacation),
+	search_button(Point(x_max()-70,0),70,20,"Search",cb_search),
+	add_tag_button(Point(x_max()-70,0),70,20,"Add",cb_add_tag),
+	add_url_button(Point(x_max()-70,0),70,20,"Add URL",cb_add_url),
+	add_file_button(Point(x_max()-70,0),70,20,"Add File",cb_add_file)
 
    //search(Point)x_max()- */
    
@@ -205,7 +205,7 @@ void Display_Window::add_tag()
 	{
 
 	}
-void Display_Window::addurl()
+void Display_Window::add_url()
 	{
 
 	}
