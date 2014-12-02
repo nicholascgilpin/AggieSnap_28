179476fc2bf16fc30078dd284a15a20f1703e2d7
ntorres16 <ntorres@tamu.edu>
2014-12-02 16:59:18 -0600

    added main to display.cpp

--- display.cpp
+++ display.cpp
@@ -224,10 +224,27 @@ void Display_Window::add_file()
 	{
 
 	}
+int main()
+{
+	try
+	{
+		if (H112 != 201401L)error("Error: incorrect std_lib_facilities_4.h version ", H112);
+		Display_Window w(Point(100, 200), 800, 1000, "Aggie Snap!");
+		return 0;
+	}
+	catch (exception& e)
+	{
+		cerr << "exception: " << e.what() << '\n';
+		return 1;
+	}
+	catch (...)
+	{
+		cerr << "Some exception\n";
+		return 2;
+		//testing
 
-
-
-
+	}
+}
 
 
 
